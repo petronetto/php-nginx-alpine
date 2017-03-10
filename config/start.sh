735fd415c78c7e1f3de642f5f84cc6b848e400e5
Juliano Petronetto <petronetto@xdevel.com.br>
2017-03-10 15:50:14 -0300

    Remove chown

--- config/start.sh
+++ config/start.sh
@@ -4,8 +4,5 @@
 procs=$(cat /proc/cpuinfo | grep processor | wc -l)
 sed -i -e "s/worker_processes 5/worker_processes $procs/" /etc/nginx/nginx.conf
 
-# Again set the right permissions (needed when mounting from a volume)
-chown -Rf www-data:www-data /var/www/src
-
 # Start supervisord and services
 exec supervisord -c /etc/supervisord.conf
